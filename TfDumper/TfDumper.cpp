865f8bd73d24c0e1f2292d7d622a2bbfa072e4e9
Dinh Ngoc Tu <none@none>
2018-07-20 21:23:44 +0200

    show default language profiles in TfDumper

--- TfDumper/TfDumper.cpp
+++ TfDumper/TfDumper.cpp
@@ -6,32 +6,36 @@
 
 #define CHECK(hr, fmt, ...) if (FAILED(hr)) { wprintf(L"HRESULT error %lx: " fmt "\n", hr, __VA_ARGS__); return hr; }
 
+std::wstring CppStringFromIID(IID const& iid) {
+    CComHeapPtr<OLECHAR> ret;
+    HRESULT hr = StringFromCLSID(iid, &ret);
+    if (FAILED(hr)) {
+        return std::wstring(L"FAILED");
+    }
+    return std::wstring(ret);
+}
+
 void PrintProfileInfo(TF_INPUTPROCESSORPROFILE const *p) {
     if (p->dwProfileType == TF_PROFILETYPE_INPUTPROCESSOR) {
         wprintf(L"TF_PROFILETYPE_INPUTPROCESSOR ");
     } else if (p->dwProfileType == TF_PROFILETYPE_KEYBOARDLAYOUT) {
         wprintf(L"TF_PROFILETYPE_KEYBOARDLAYOUT ");
     }
-    wprintf(L"0x%x:\n", static_cast<int>(p->langid));
+    wprintf(L"0x%04x:\n", static_cast<int>(p->langid));
 
-    CComHeapPtr<OLECHAR> clsid;
-    StringFromCLSID(p->clsid, &clsid);
-    CComHeapPtr<OLECHAR> guidProfile;
-    StringFromCLSID(p->guidProfile, &guidProfile);
-    wprintf(L"clsid %s guidProfile %s\n", static_cast<PWSTR>(clsid), static_cast<PWSTR>(guidProfile));
+    wprintf(L"clsid %s guidProfile %s\n", CppStringFromIID(p->clsid).c_str(), CppStringFromIID(p->guidProfile).c_str());
 
-    CComHeapPtr<OLECHAR> catid;
-    StringFromCLSID(p->catid, &catid);
+    std::wstring catid = CppStringFromIID(p->catid);
     if (IsEqualGUID(p->catid, GUID_TFCAT_TIP_KEYBOARD)) {
-        wprintf(L"catid %s (GUID_TFCAT_TIP_KEYBOARD)\n", static_cast<PWSTR>(catid));
+        wprintf(L"catid %s (GUID_TFCAT_TIP_KEYBOARD)\n", catid.c_str());
     } else if (IsEqualGUID(p->catid, GUID_TFCAT_TIP_SPEECH)) {
-        wprintf(L"catid %s (GUID_TFCAT_TIP_SPEECH)\n", static_cast<PWSTR>(catid));
+        wprintf(L"catid %s (GUID_TFCAT_TIP_SPEECH)\n", catid.c_str());
     } else if (IsEqualGUID(p->catid, GUID_TFCAT_TIP_HANDWRITING)) {
-        wprintf(L"catid %s (GUID_TFCAT_TIP_HANDWRITING)\n", static_cast<PWSTR>(catid));
+        wprintf(L"catid %s (GUID_TFCAT_TIP_HANDWRITING)\n", catid.c_str());
     } else if (IsEqualGUID(p->catid, GUID_TFCAT_CATEGORY_OF_TIP)) {
-        wprintf(L"catid %s (GUID_TFCAT_CATEGORY_OF_TIP)\n", static_cast<PWSTR>(catid));
+        wprintf(L"catid %s (GUID_TFCAT_CATEGORY_OF_TIP)\n", catid.c_str());
     } else {
-        wprintf(L"catid %s (unknown category)\n", static_cast<PWSTR>(catid));
+        wprintf(L"catid %s (unknown category)\n", catid.c_str());
     }
 
     wprintf(L"hkl %p\n", p->hkl);
@@ -105,6 +109,37 @@ HRESULT EnumProfileMgr() {
     return S_OK;
 }
 
+HRESULT EnumDefaultKeyboardLanguageProfiles() {
+    HRESULT hr;
+
+    SmartComPtr<ITfInputProcessorProfiles> profiles;
+    hr = profiles.CoCreate(CLSID_TF_InputProcessorProfiles, NULL, CLSCTX_INPROC_SERVER);
+    CHECK(hr, L"%s", L"profiles.CoCreate");
+
+    CComHeapPtr<LANGID> langids;
+    ULONG numlangids;
+    hr = profiles->GetLanguageList(&langids, &numlangids);
+    CHECK(hr, L"%s", L"profiles->GetLanguageList");
+
+    for (ULONG i = 0; i < numlangids; i++) {
+        LANGID lid = (static_cast<LANGID *>(langids))[i];
+        wprintf(L"0x%04x: ", lid);
+        CLSID clsid;
+        GUID guidProfile;
+        hr = profiles->GetDefaultLanguageProfile(lid, GUID_TFCAT_TIP_KEYBOARD, &clsid, &guidProfile);
+        CHECK(hr, L"%s", L"profiles->GetDefaultLanguageProfile");
+        if (hr == S_FALSE) {
+            wprintf(L"no default\n");
+        } else {
+            wprintf(L"clsid %s guidProfile %s\n", CppStringFromIID(clsid).c_str(), CppStringFromIID(guidProfile).c_str());
+        }
+    }
+
+    wprintf(L"\n");
+
+    return S_OK;
+}
+
 int main() {
     HRESULT hr;
 
@@ -113,6 +148,10 @@ int main() {
 
     hr = EnumProfileMgr();
     CHECK(hr, L"%s", L"EnumProfileMgr");
+
+    wprintf(L"Default keyboard language profiles:\n");
+    hr = EnumDefaultKeyboardLanguageProfiles();
+    CHECK(hr, L"%s", L"EnumDefaultKeyboardLanguageProfiles");
 
     CoUninitialize();
 
