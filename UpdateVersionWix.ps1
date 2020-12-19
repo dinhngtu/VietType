87ce302f492be8e82b8bc7b70a66d978bbf8c2e9
dinhngtu <dinhngtu@users.noreply.github.com>
2020-12-19 23:10:21 +0100

    Create automated workflow (#1)

--- UpdateVersionWix.ps1
+++ UpdateVersionWix.ps1
@@ -3,7 +3,7 @@ param([switch] $Force)
 Get-Content "$PSScriptRoot\Version.ps1" | Invoke-Expression
 
 # compile happens in bin\Configuration folder
-$vcsRev = git describe --tags --dirty
+$vcsRev = git describe --tags --dirty --always
 $wixFile = "..\..\Version.wxi"
 $wixOld = Get-Content -ErrorAction Ignore -Raw $wixFile
 $wixNew = `
@@ -11,11 +11,11 @@ $wixNew = `
 <?xml version="1.0" encoding="utf-8"?>
 <Include>
   <?define productVersion="$($productVersion[0]).$($productVersion[1]).$($productVersion[2]).$($productVersion[3])" ?>
-  <?define vcsRev="$vcsRev" ?>
+  <?define vcsRev="Rev-$vcsRev" ?>
 </Include>
 
 "@
 if ($Force -or ($wixOld -ne $wixNew)) {
-    echo "Updating Wix version include"
+    echo "Updating Wix version include: $vcsRev"
     [System.IO.File]::WriteAllText($wixFile, [string]::Join("`n", $wixNew))
 }
