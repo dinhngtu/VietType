a0a60a7c15ca11319cf153e241336c2e3ea90cfc
Tu Dinh <dinhngtu@users.noreply.github.com>
2021-11-25 17:45:07 +0100

    fix build scripts

--- build64.bat
+++ build64.bat
@@ -1,2 +1,2 @@
-msbuild.exe VietType.sln -target:VietTypeRegistrar:Rebuild;VietTypeATL:Rebuild -p:Configuration=Release -p:Platform=Win32
+msbuild.exe VietType.sln -target:VietTypeATL:Rebuild -p:Configuration=Release -p:Platform=Win32
 msbuild.exe VietType.sln -target:VietTypeSetup:Rebuild -p:Configuration=Release -p:Platform=x64
--- build64d.bat
+++ build64d.bat
@@ -1,2 +1,2 @@
-msbuild.exe VietType.sln -target:VietTypeRegistrar;VietTypeATL -p:Configuration=Debug -p:Platform=Win32
+msbuild.exe VietType.sln -target:VietTypeATL -p:Configuration=Debug -p:Platform=Win32
 msbuild.exe VietType.sln -target:VietTypeSetup -p:Configuration=Debug -p:Platform=x64
