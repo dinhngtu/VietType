f7e25d8b3ac14faaab648fffa6444b44492317e7
Tu Dinh <dinhngtu@users.noreply.github.com>
2022-08-15 01:21:39 +0200

    clean up resource file

--- VietTypeATL/resource.h
+++ VietTypeATL/resource.h
@@ -7,13 +7,7 @@
 #define IDS_ABOUTSTRING                 101
 #define IDS_VIETTYPE_DESCRIPTION        102
 #define IDR_TEXTSERVICE                 106
-#define IDD_SETTINGS                    107
 #define IDI_IMELOGO                     202
-#define IDC_SETTINGS_OA_UY              203
-#define IDC_SETTINGS_ACCEPT_DD          204
-#define IDC_SETTINGS_DEFAULT_ENABLED    205
-#define IDC_SETTINGS_BACKSPACE_INVALID  206
-#define IDC_SETTINGS_BACKCONVERT_ON_BACKSPACE 207
 #define IDR_MENU_TRAY                   213
 #define IDI_ICONED                      220
 #define IDI_ICONEL                      221
