14ae8a74316689021769a761c917a420ab200c0d
Tu Dinh <dinhngtu@users.noreply.github.com>
2022-08-21 05:27:23 +0200

    config: retarget to 4.5.2

--- VietTypeConfig/Program.cs
+++ VietTypeConfig/Program.cs
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace VietTypeConfig {
