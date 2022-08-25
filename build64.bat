df0045d4e362fb3be8ad8619dda1f3a30ff44ef0
Tu Dinh <dinhngtu@users.noreply.github.com>
2022-08-25 23:07:35 +0200

    cleanup unused files

--- build64.bat
+++
@@ -1,2 +0,0 @@
-msbuild.exe VietType.sln -target:VietTypeATL:Rebuild -p:Configuration=Release -p:Platform=Win32
-msbuild.exe VietType.sln -target:VietTypeSetup:Rebuild -p:Configuration=Release -p:Platform=x64
--- build64d.bat
+++
@@ -1,2 +0,0 @@
-msbuild.exe VietType.sln -target:VietTypeATL -p:Configuration=Debug -p:Platform=Win32
-msbuild.exe VietType.sln -target:VietTypeSetup -p:Configuration=Debug -p:Platform=x64
