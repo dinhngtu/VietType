c4b9374490675e4ded88d15c88c0b4877754ff25
Tu Dinh <dinhngtu@users.noreply.github.com>
2022-10-25 21:13:56 +0200

    config: anchor separator label

--- VietTypeConfig/Form1.Designer.cs
+++ VietTypeConfig/Form1.Designer.cs
@@ -154,6 +154,8 @@ namespace VietTypeConfig {
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label1.Location = new System.Drawing.Point(0, 42);
             this.label1.Name = "label1";
