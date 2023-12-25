756ce5a5ea81344927d815970f46fdf4913f2f59
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2023-12-25 19:37:29 +0100

    v1.8.7 [skip ci]

--- Version.ps1
+++ Version.ps1
@@ -1,5 +1,5 @@
-$fileVersion = 1,8,6,0
-$productVersion = 1,8,6,0
+$fileVersion = 1,8,7,0
+$productVersion = 1,8,7,0
 
 if ($Env:GITHUB_ACTIONS -eq "true") {
     $fileVersion[3] = [int]$Env:GITHUB_RUN_NUMBER
