5cc012c8ddff75986f69ff01189073c83eb32d3b
dinhngtu <1257909+dinhngtu@users.noreply.github.com>
2025-03-25 06:08:45 +0100

    v1.11.2

--- Version.ps1
+++ Version.ps1
@@ -1,5 +1,5 @@
-$fileVersion = 1,10,4,0
-$productVersion = 1,10,4,0
+$fileVersion = 1,11,2,0
+$productVersion = 1,11,2,0
 
 if ($Env:GITHUB_ACTIONS -eq "true") {
     $fileVersion[3] = [int]$Env:GITHUB_RUN_NUMBER
