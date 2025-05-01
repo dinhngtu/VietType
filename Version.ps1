d16189f5a8b36dc4e09a512816e876bb81f35b1c
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2025-05-01 13:51:50 +0200

    fix broken release versioning once and for all

--- Version.ps1
+++ Version.ps1
@@ -1,7 +1,24 @@
-$fileVersion = 1,11,5,0
-$productVersion = 1,11,5,0
+$fileVersion = 1, 11, 5, 0
+$productVersion = 1, 11, 5, 0
 
 if ($Env:GITHUB_ACTIONS -eq "true") {
     $fileVersion[3] = [int]$Env:GITHUB_RUN_NUMBER
     $productVersion[3] = [int]$Env:GITHUB_RUN_NUMBER
+
+    if ($Env:GITHUB_REF_NAME -match '^v(\d+)(?:\.(\d+))?(?:\.(\d+))?(?:\.(\d+))?$') {
+        $fileVersion[0] = [int]$Matches[1]
+        $productVersion[0] = [int]$Matches[1]
+        if ($null -ne $Matches[2]) {
+            $fileVersion[1] = [int]$Matches[2]
+            $productVersion[1] = [int]$Matches[2]
+        }
+        if ($null -ne $Matches[3]) {
+            $fileVersion[2] = [int]$Matches[3]
+            $productVersion[2] = [int]$Matches[3]
+        }
+        if ($null -ne $Matches[4]) {
+            $fileVersion[3] = [int]$Matches[4]
+            $productVersion[3] = [int]$Matches[4]
+        }
+    }
 }
