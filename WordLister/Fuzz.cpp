6cbe98e2cb5ecf1855e20148bc228df338f87dc4
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2025-06-22 17:06:36 +0200

    rework fuzzer
    minimize fuzzer corpus
    use hardware threads for better concurrency
    
    Signed-off-by: Tu Dinh <1257909+dinhngtu@users.noreply.github.com>

--- WordLister/Fuzz.cpp
+++ WordLister/Fuzz.cpp
@@ -3,44 +3,58 @@
 
 #include "stdafx.h"
 #include <thread>
+#include <mutex>
+#include <deque>
 #include "Telex.h"
 #include "TelexEngine.h"
 
 using namespace VietType::Telex;
 
-static constexpr const int skip = 4;
-static constexpr const std::wstring_view table = L"abcdefghijklmnopqrstuvwxyzACDEFGHJKLMNOPQRSTUVWXYZ0123456789[]{}";
+struct FuzzWorkItem {
+    int len;
+    TypingStyles style;
+    std::wstring_view table;
+    int start;
+    int end;
+};
 
-static void DoFuzz(int len, int start = 0, int end = table.size()) {
+static constexpr const int skip = 1;
+static constexpr const std::wstring_view table_telex = L"acdefghinoqsuwyz";
+static constexpr const std::wstring_view table_telex_complicated = L"acdefghinoqsuwyz[]";
+static constexpr const std::wstring_view table_vni = L"acdeghinoquy0126789";
+
+template <size_t table_size>
+static void DoFuzz(int len, TypingStyles style, const std::wstring_view& table, int start = 0, int end = table_size) {
+    if (table.size() != table_size) {
+        throw std::runtime_error("invalid table size");
+    }
     for (int level = 0; level <= 3; level++) {
         for (int autocorrect = 0; autocorrect <= 1; autocorrect++) {
-            for (int style = 0; style < (int)TypingStyles::Max; style++) {
-                wprintf(L"len %d level %d autocorrect %d\n", len, level, autocorrect);
-                TelexConfig config;
-                config.optimize_multilang = level;
-                config.autocorrect = !!autocorrect;
-                config.typing_style = (TypingStyles)style;
-                TelexEngine e(config);
-                for (int prefix = start; prefix < end; prefix++) {
-                    size_t max = 1;
-                    for (auto i = 1; i < len; i++) {
-                        max *= table.size();
+            wprintf(L"len %d level %d autocorrect %d\n", len, level, autocorrect);
+            TelexConfig config;
+            config.optimize_multilang = level;
+            config.autocorrect = !!autocorrect;
+            config.typing_style = style;
+            TelexEngine e(config);
+            for (int prefix = start; prefix < end; prefix++) {
+                size_t max = 1;
+                for (auto i = 1; i < len; i++) {
+                    max *= table_size;
+                }
+                for (size_t i = 0; i < max; i++) {
+                    e.Reset();
+                    size_t cur = i;
+                    e.PushChar(table[prefix]);
+                    for (auto j = 1; j < len; j++) {
+                        e.PushChar(table[cur % table_size]);
+                        cur /= table_size;
                     }
-                    for (size_t i = 0; i < max; i++) {
-                        e.Reset();
-                        size_t cur = i;
-                        e.PushChar(table[prefix]);
-                        for (auto j = 1; j < len; j++) {
-                            e.PushChar(table[cur % table.size()]);
-                            cur /= table.size();
-                        }
-                        if (!e.CheckInvariants()) {
-                            wprintf(L"word failed: %s\n", e.RetrieveRaw().c_str());
-                        }
-                        e.Commit();
-                        if (!e.CheckInvariants()) {
-                            wprintf(L"word failed commit: %s\n", e.RetrieveRaw().c_str());
-                        }
+                    if (!e.CheckInvariants()) {
+                        wprintf(L"word failed: %s\n", e.RetrieveRaw().c_str());
+                    }
+                    e.Commit();
+                    if (!e.CheckInvariants()) {
+                        wprintf(L"word failed commit: %s\n", e.RetrieveRaw().c_str());
                     }
                 }
             }
@@ -48,21 +62,86 @@ static void DoFuzz(int len, int start = 0, int end = table.size()) {
     }
 }
 
-static void DoFuzzThreaded(int len) {
-    std::vector<std::jthread> workers;
-    for (int i = 0; i < table.size(); i += skip) {
-        workers.emplace_back(DoFuzz, len, i, std::min(i + skip, (int)table.size()));
+static void FuzzWorker(std::deque<FuzzWorkItem>* wq, std::mutex* wq_lock) {
+    while (1) {
+        FuzzWorkItem wi;
+        {
+            std::lock_guard guard(*wq_lock);
+            if (wq->empty())
+                break;
+            wi = wq->front();
+            wq->pop_front();
+        }
+        if (wi.table.size() == table_telex.size()) {
+            DoFuzz<table_telex.size()>(wi.len, wi.style, wi.table, wi.start, wi.end);
+        } else if (wi.table.size() == table_telex_complicated.size()) {
+            DoFuzz<table_telex_complicated.size()>(wi.len, wi.style, wi.table, wi.start, wi.end);
+        } else if (wi.table.size() == table_vni.size()) {
+            DoFuzz<table_vni.size()>(wi.len, wi.style, wi.table, wi.start, wi.end);
+        }
     }
 }
 
 bool fuzz() {
     SetPriorityClass(GetCurrentProcess(), IDLE_PRIORITY_CLASS);
     SetThreadExecutionState(ES_SYSTEM_REQUIRED | ES_CONTINUOUS);
-    for (auto len = 1; len <= 3; len++) {
-        DoFuzz(len);
+    {
+        std::deque<FuzzWorkItem> wq;
+        std::mutex wq_lock;
+        for (auto len = 1; len <= 5; len++) {
+            wq.emplace_back(FuzzWorkItem{
+                .len = len,
+                .style = TypingStyles::Telex,
+                .table = table_telex,
+                .start = 0,
+                .end = (int)table_telex.size(),
+            });
+            wq.emplace_back(FuzzWorkItem{
+                .len = len,
+                .style = TypingStyles::TelexComplicated,
+                .table = table_telex_complicated,
+                .start = 0,
+                .end = (int)table_telex_complicated.size(),
+            });
+            wq.emplace_back(FuzzWorkItem{
+                .len = len,
+                .style = TypingStyles::Vni,
+                .table = table_vni,
+                .start = 0,
+                .end = (int)table_vni.size(),
+            });
+        }
+        for (auto len = 6; len <= 8; len++) {
+            for (int i = 0; i < table_telex.size(); i += skip) {
+                wq.emplace_back(FuzzWorkItem{
+                    .len = len,
+                    .style = TypingStyles::Telex,
+                    .table = table_telex,
+                    .start = i,
+                    .end = std::min(i + skip, (int)table_telex.size()),
+                });
+                wq.emplace_back(FuzzWorkItem{
+                    .len = len,
+                    .style = TypingStyles::TelexComplicated,
+                    .table = table_telex_complicated,
+                    .start = i,
+                    .end = std::min(i + skip, (int)table_telex_complicated.size()),
+                });
+                wq.emplace_back(FuzzWorkItem{
+                    .len = len,
+                    .style = TypingStyles::Vni,
+                    .table = table_vni,
+                    .start = i,
+                    .end = std::min(i + skip, (int)table_vni.size()),
+                });
+            }
+        }
+
+        std::vector<std::jthread> workers;
+        for (unsigned int i = 0; i < std::thread::hardware_concurrency(); i++) {
+            workers.emplace_back(FuzzWorker, &wq, &wq_lock);
+        }
     }
-    for (auto len = 4; len <= 7; len++) {
-        DoFuzzThreaded(len);
-    }
+    SetThreadExecutionState(ES_CONTINUOUS);
     return true;
 }
