d3da14876471ca79774abb659803701a6c5f18f7
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2025-06-22 17:31:56 +0200

    fuzz test backspacing
    Signed-off-by: Tu Dinh <1257909+dinhngtu@users.noreply.github.com>

--- WordLister/Fuzz.cpp
+++ WordLister/Fuzz.cpp
@@ -11,11 +11,12 @@
 using namespace VietType::Telex;
 
 struct FuzzWorkItem {
-    int len;
-    TypingStyles style;
-    std::wstring_view table;
-    int start;
-    int end;
+    int len = 0;
+    TypingStyles style = TypingStyles::Max;
+    std::wstring_view table{};
+    int start = 0;
+    int end = 0;
+    int mode = 0;
 };
 
 static constexpr const int skip = 1;
@@ -24,37 +25,57 @@ static constexpr const std::wstring_view table_telex_complicated = L"acdefghinoq
 static constexpr const std::wstring_view table_vni = L"acdeghinoquy0126789";
 
 template <size_t table_size>
-static void DoFuzz(int len, TypingStyles style, const std::wstring_view& table, int start = 0, int end = table_size) {
-    if (table.size() != table_size) {
+static void DoFuzz(const FuzzWorkItem& wi) {
+    if (wi.table.size() != table_size) {
         throw std::runtime_error("invalid table size");
     }
     for (int level = 0; level <= 3; level++) {
         for (int autocorrect = 0; autocorrect <= 1; autocorrect++) {
-            wprintf(L"len %d level %d autocorrect %d\n", len, level, autocorrect);
+            wprintf(
+                L"len %d style %d ts %zu start %d end %d mode %d level %d autocorrect %d\n",
+                wi.len,
+                (int)wi.style,
+                wi.table.size(),
+                wi.start,
+                wi.end,
+                wi.mode,
+                level,
+                autocorrect);
             TelexConfig config;
             config.optimize_multilang = level;
             config.autocorrect = !!autocorrect;
-            config.typing_style = style;
+            config.typing_style = wi.style;
             TelexEngine e(config);
-            for (int prefix = start; prefix < end; prefix++) {
+            for (int prefix = wi.start; prefix < wi.end; prefix++) {
                 size_t max = 1;
-                for (auto i = 1; i < len; i++) {
+                for (auto i = 1; i < wi.len; i++) {
                     max *= table_size;
                 }
                 for (size_t i = 0; i < max; i++) {
                     e.Reset();
                     size_t cur = i;
-                    e.PushChar(table[prefix]);
-                    for (auto j = 1; j < len; j++) {
-                        e.PushChar(table[cur % table_size]);
+                    e.PushChar(wi.table[prefix]);
+                    for (auto j = 1; j < wi.len; j++) {
+                        e.PushChar(wi.table[cur % table_size]);
                         cur /= table_size;
                     }
                     if (!e.CheckInvariants()) {
                         wprintf(L"word failed: %s\n", e.RetrieveRaw().c_str());
                     }
-                    e.Commit();
-                    if (!e.CheckInvariants()) {
-                        wprintf(L"word failed commit: %s\n", e.RetrieveRaw().c_str());
+                    if (wi.mode == 0) {
+                        if (e.Commit() == TelexStates::TxError) {
+                            wprintf(L"word failed commit: %s\n", e.RetrieveRaw().c_str());
+                        }
+                        if (!e.CheckInvariants()) {
+                            wprintf(L"word failed commit invariant: %s\n", e.RetrieveRaw().c_str());
+                        }
+                    } else {
+                        if (e.Backspace() == TelexStates::TxError) {
+                            wprintf(L"word failed backspace: %s\n", e.RetrieveRaw().c_str());
+                        }
+                        if (!e.CheckInvariants()) {
+                            wprintf(L"word failed backspace invariant: %s\n", e.RetrieveRaw().c_str());
+                        }
                     }
                 }
             }
@@ -73,11 +94,13 @@ static void FuzzWorker(std::deque<FuzzWorkItem>* wq, std::mutex* wq_lock) {
             wq->pop_front();
         }
         if (wi.table.size() == table_telex.size()) {
-            DoFuzz<table_telex.size()>(wi.len, wi.style, wi.table, wi.start, wi.end);
+            DoFuzz<table_telex.size()>(wi);
         } else if (wi.table.size() == table_telex_complicated.size()) {
-            DoFuzz<table_telex_complicated.size()>(wi.len, wi.style, wi.table, wi.start, wi.end);
+            DoFuzz<table_telex_complicated.size()>(wi);
         } else if (wi.table.size() == table_vni.size()) {
-            DoFuzz<table_vni.size()>(wi.len, wi.style, wi.table, wi.start, wi.end);
+            DoFuzz<table_vni.size()>(wi);
+        } else {
+            throw std::runtime_error("invalid table size");
         }
     }
 }
@@ -89,51 +112,61 @@ bool fuzz() {
         std::deque<FuzzWorkItem> wq;
         std::mutex wq_lock;
         for (auto len = 1; len <= 5; len++) {
-            wq.emplace_back(FuzzWorkItem{
-                .len = len,
-                .style = TypingStyles::Telex,
-                .table = table_telex,
-                .start = 0,
-                .end = (int)table_telex.size(),
-            });
-            wq.emplace_back(FuzzWorkItem{
-                .len = len,
-                .style = TypingStyles::TelexComplicated,
-                .table = table_telex_complicated,
-                .start = 0,
-                .end = (int)table_telex_complicated.size(),
-            });
-            wq.emplace_back(FuzzWorkItem{
-                .len = len,
-                .style = TypingStyles::Vni,
-                .table = table_vni,
-                .start = 0,
-                .end = (int)table_vni.size(),
-            });
-        }
-        for (auto len = 6; len <= 8; len++) {
-            for (int i = 0; i < table_telex.size(); i += skip) {
+            for (auto mode = 0; mode <= 1; mode++) {
                 wq.emplace_back(FuzzWorkItem{
                     .len = len,
                     .style = TypingStyles::Telex,
                     .table = table_telex,
-                    .start = i,
-                    .end = std::min(i + skip, (int)table_telex.size()),
+                    .start = 0,
+                    .end = (int)table_telex.size(),
+                    .mode = mode,
                 });
                 wq.emplace_back(FuzzWorkItem{
                     .len = len,
                     .style = TypingStyles::TelexComplicated,
                     .table = table_telex_complicated,
-                    .start = i,
-                    .end = std::min(i + skip, (int)table_telex_complicated.size()),
+                    .start = 0,
+                    .end = (int)table_telex_complicated.size(),
+                    .mode = mode,
                 });
                 wq.emplace_back(FuzzWorkItem{
                     .len = len,
                     .style = TypingStyles::Vni,
                     .table = table_vni,
-                    .start = i,
-                    .end = std::min(i + skip, (int)table_vni.size()),
+                    .start = 0,
+                    .end = (int)table_vni.size(),
+                    .mode = mode,
                 });
+            }
+        }
+        for (auto len = 6; len <= 8; len++) {
+            for (int i = 0; i < table_telex.size(); i += skip) {
+                for (auto mode = 0; mode <= 1; mode++) {
+                    wq.emplace_back(FuzzWorkItem{
+                        .len = len,
+                        .style = TypingStyles::Telex,
+                        .table = table_telex,
+                        .start = i,
+                        .end = std::min(i + skip, (int)table_telex.size()),
+                        .mode = mode,
+                    });
+                    wq.emplace_back(FuzzWorkItem{
+                        .len = len,
+                        .style = TypingStyles::TelexComplicated,
+                        .table = table_telex_complicated,
+                        .start = i,
+                        .end = std::min(i + skip, (int)table_telex_complicated.size()),
+                        .mode = mode,
+                    });
+                    wq.emplace_back(FuzzWorkItem{
+                        .len = len,
+                        .style = TypingStyles::Vni,
+                        .table = table_vni,
+                        .start = i,
+                        .end = std::min(i + skip, (int)table_vni.size()),
+                        .mode = mode,
+                    });
+                }
             }
         }
 
