6e0f4a5ad15476247c92444bfd4b9626c8bab690
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2026-03-16 16:30:07 +0100

    move EditKey into EditSessions.cpp
    Now that things have been cleaned up, we don't need a separate
    KeyHandler.cpp any more.
    
    Signed-off-by: Tu Dinh <1257909+dinhngtu@users.noreply.github.com>

--- VietTypeATL/EditSessions.cpp
+++ VietTypeATL/EditSessions.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include "Context.h"
+#include "KeyTranslator.h"
 
 namespace VietType {
 
@@ -261,6 +262,39 @@ HRESULT Context::DoEditNextState(
     }
 
     return S_OK;
+}
+
+HRESULT Context::EditKey(_In_ TfEditCookie ec, _In_ Context* context, _In_ KeyResult keyResult, _In_ wchar_t push) {
+    DBG_DPRINT(L"KeyHandler ec = %ld %s '%c'", ec, GetKeyResult(keyResult), push ? push : L'?');
+
+    HRESULT hr;
+    Telex::ITelexEngine* engine = context->GetEngine();
+
+    CComPtr<ITfComposition> composition;
+    hr = context->GetComposition(ec, &composition);
+    HRESULT_CHECK_RETURN(hr, L"context->GetComposition failed");
+
+    // recover from situations that terminate the composition without us knowing (e.g. mouse clicks)
+    if (!composition && engine->Count()) {
+        DBG_DPRINT("resetting");
+        engine->Reset();
+    }
+
+    switch (keyResult) {
+    case KeyResult::BreakingCharacter:
+        return context->DoEditNextState(ec, composition, engine->Commit(), push);
+    case KeyResult::Character:
+        return context->DoEditNextState(ec, composition, engine->PushChar(push), L'\0');
+    case KeyResult::Backspace:
+        return context->DoEditNextState(ec, composition, engine->Backspace(), L'\0');
+    case KeyResult::Escape:
+        return context->DoEditNextState(ec, composition, engine->Cancel(), L'\0');
+    case KeyResult::NotEaten:
+    case KeyResult::NotEatenEndComposition:
+    default:
+        engine->Reset();
+        return context->EndCompositionNow(ec, composition);
+    }
 }
 
 } // namespace VietType
