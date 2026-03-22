0b50b5e0aed679bb86b9a80dacf78e4bd8fce477
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2026-03-22 04:47:44 +0100

    add quick update script for testing
    Signed-off-by: Tu Dinh <1257909+dinhngtu@users.noreply.github.com>

---
+++ update.cmd
@@ -0,0 +1,23 @@
+@echo off
+:start
+echo For testing purposes only!
+xcopy /y "%~dp0\x64\Release\VietTypeATL64.dll" "%ProgramFiles%\VietType\VietTypeATL64.dll"
+if %errorlevel% neq 0 goto :failed
+xcopy /y "%~dp0\x64\Release\VietTypeATL64.pdb" "%ProgramFiles%\VietType\VietTypeATL64.pdb"
+if %errorlevel% neq 0 goto :failed
+xcopy /y "%~dp0\Win32\Release\VietTypeATL32.dll" "%ProgramFiles(x86)%\VietType\VietTypeATL32.dll"
+if %errorlevel% neq 0 goto :failed
+xcopy /y "%~dp0\Win32\Release\VietTypeATL32.pdb" "%ProgramFiles(x86)%\VietType\VietTypeATL32.pdb"
+if %errorlevel% neq 0 goto :failed
+
+echo Update successful.
+pause
+exit /b 0
+
+:failed
+echo.
+echo Update failed!
+set /p retry="Do you want to try again? (Y/N): "
+if /i "%retry%"=="Y" goto :start
+
+exit /b 1
